03f243d9c9e85cd21535c6353cc9073b34804e69
trinker <tyler.rinker@gmail.com>
2014-08-20 22:01:41 -0400

    added html check

--- R/shortcut.R
+++ R/shortcut.R
@@ -5,13 +5,15 @@
 #' @param url A url to redirect to.
 #' @param file The name of the output file.  Defaults to the console.
 #' open A name to use for the file (.html extension).
+#' @param check.ext logical.  If \code{TRUE} check for and add an HTML file 
+#' extension to file.
 #' @export
 #' @examples
 #' shortcut("http://www.talkstats.com/")
 #' \dontrun{
 #' shortcut("http://trinker.github.io/card/contact")
 #' }
-shortcut <- function(url=read_clip(), file = "", open = TRUE) { 
+shortcut <- function(url=read_clip(), file = "", open = TRUE, check.ext = TRUE) { 
 	message("Attempting to create shortcut\n")
 		
     root <- c("<!DOCTYPE html>", "<html>", "<head>", "<script>", "</script>", 
@@ -19,6 +21,10 @@ shortcut <- function(url=read_clip(), file = "", open = TRUE) {
         "<body>", "    </body>", "</html>")
     
     out <- sprintf(paste(root, collapse="\n"), url)
+    
+    if (check.ext && tools::file_ext(file) != "html") {
+        file <- paste0(file, ".html")
+    }
     
     cat(out, file=file)
 
